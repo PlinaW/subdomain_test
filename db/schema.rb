f15869a91eb7262f30d911f2c0593fcfd5a48cf9
PlinaW <plinski.wojciech@gmail.com>
2024-08-13 17:06:42 +0200

    Add skills to users and user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_13_141156) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_13_145751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_13_141156) do
     t.string "work_history"
     t.string "languages"
     t.text "hobbies"
+    t.text "skills"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
