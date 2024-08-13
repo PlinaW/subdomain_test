abfa7c1b3d3ebafb1da14c4df00670a59fffa0c3
PlinaW <plinski.wojciech@gmail.com>
2024-08-13 16:51:00 +0200

    Add hobbies to users and show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_12_170526) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_13_141156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_12_170526) do
     t.string "education"
     t.string "work_history"
     t.string "languages"
+    t.text "hobbies"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
