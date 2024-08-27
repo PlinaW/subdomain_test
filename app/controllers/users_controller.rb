57521ff48f07002b529e790adb760015a43219f2
PlinaW <110122241+PlinaW@users.noreply.github.com>
2024-08-27 19:24:22 +0200

    User edit page (#18)
    * Change password form
    
    * User update
    
    * User edit forms
    
    * Add user photo to user edit page
    
    * User edit layout repaired

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,41 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update ]
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User was updated successfully"
+      redirect_to user_url(subdomain: @user.site_users.first.site.subdomain), allow_other_host: true
+    else
+      render edit
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name,
+                                 :last_name,
+                                 :phone_number,
+                                 :country,
+                                 :city,
+                                 :zip_code,
+                                 :address,
+                                 :birthday,
+                                 :languages,
+                                 :education,
+                                 :work_history,
+                                 :about_me,
+                                 :hobbies,
+                                 :skills)
   end
 end
