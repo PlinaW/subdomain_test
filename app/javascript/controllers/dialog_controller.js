a748a4ce3331001f9e2f24386612f96f14696fe6
PlinaW <110122241+PlinaW@users.noreply.github.com>
2024-09-11 20:53:36 +0200

    Update table with turbo stream after user invitation (#24)

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -3,6 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "modal" ]
 
+    // hide modal on successful form submission
+  // data-action="turbo:submit-end->turbo-modal#submitEnd"
+  submitEnd(e) {
+    if (e.detail.success) {
+      // console.log(e.detail.success)
+      this.modalTarget.close()
+    }
+  }
+
   open() {
     this.modalTarget.showModal();
     document.body.classList.add("overflow-hidden");
