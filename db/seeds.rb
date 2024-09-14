b99c5988d0fda1b5c746eaf0d1d414e9663c214b
PlinaW <plinski.wojciech@gmail.com>
2024-09-14 14:42:27 +0200

    Add faker gem, and fake accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# 20.times do
+#   User.create(email: Faker::Internet.email(domain: 'test.com'),
+#               password: 'password',
+#               first_name: Faker::Name.first_name,
+#               last_name: Faker::Name.last_name)
+# end
+
+# User.all.each do |user|
+#   SiteUser.create(site_id: 89,
+#                   user_id: user.id)
+# end
