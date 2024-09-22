a6f5fe9ec9c0625d84596935bf98b36f21815ec5
PlinaW <plinski.wojciech@gmail.com>
2024-09-22 20:00:28 +0200

    Add avatar to users table, add new variant to user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
   belongs_to :site, optional: true
   has_many :site_users
   has_one_attached :avatar do |attachable|
-    attachable.variant :thumb, resize_to_limit: [ 200, 200 ]
+    attachable.variant :avatar, resize_to_limit: [ 200, 200 ]
+    attachable.variant :thumb, resize_to_limit: [ 50, 50 ]
   end
 
   validates :first_name, presence: true
